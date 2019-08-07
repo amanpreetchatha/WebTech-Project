90d0239b3126eb9f66f7c3b8e0d4c4e31dcf60d1
Amanpreet Chatha <50677671+amanpreetchatha@users.noreply.github.com>
2019-08-07 17:12:46 -0400

    third commit

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(function(req,res,next){
-    
+    console.log("i am in the middle");
+    console.log(req.body);
+    next();
 });
 
 
