2d3ed1b249f2c2849504091f31d4a87ab71d17b1
Amanpreet Chatha <50677671+amanpreetchatha@users.noreply.github.com>
2019-08-07 17:08:35 -0400

    third commit

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const http=require("http");
-const data={firstName: 'Amanpreet', lastName: 'Chatha'};
+var express=require('express');
+var app=express();
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(function(req,res,next){
+    
+});
+
+
+app.get('/',function(req,res){
+    res.send('Hello World');
+});
+app.post('/',function(req,res){
+    res.send('<h1>Hello World from Post</h1>');
+});
+app.put('/',function(req,res){
+    res.send('<h1>Hello World from Put</h1>');
+});
+app.delete('/',function(req,res){
+    res.send('<h1>Hello World from Delete</h1>');
+});
+
+/*const data={firstName: 'Amanpreet', lastName: 'Chatha'};
 const site=http.createServer(function(req,res){
     console.log("hello world");
     res.setHeader("Content-Type",'text/html');
@@ -8,5 +31,5 @@ const site=http.createServer(function(req,res){
     res.end('<h1>Hello World!</h1>');
     console.log(req.headers);
 
-});
-site.listen(3000);
+})*/
+app.listen(3000);
