808df4e0c48c49417a39fbc62afab9315b6dc059
Amanpreet Chatha <50677671+amanpreetchatha@users.noreply.github.com>
2019-08-13 23:27:01 -0400

    eleventh commit

--- HTTP server.js
+++ HTTP server.js
@@ -3,15 +3,16 @@ const http=require('http');
 function handler(req,res){
     
     res.setHeader('Content-Type','text/html')
+    
     if(req.method==='GET'){
-        res.write("<h1>This is a GET request</h1>");
+        res.write(`<h1>This is a GET request.<br>Date is ${Date()}</h1>`);
     }else if(req.method==='POST'){
-        res.write("<h1>This is a POST request</h1>");
+        res.write(`<h1>This is a POST request.<br>Date is ${Date()}</h1>`);
     }else if(req.method==='PUT'){
-        res.write("<h1>This is a PUT request</h1>");
+        res.write(`<h1>This is a PUT request.<br>Date is ${Date()}</h1>`);
     }
     else if(req.method='DELETE'){
-        res.write("<h1>This is a DELETE request</h1>");
+        res.write(`<h1>This is a DELETE request.<br>Date is ${Date()}</h1>`);
     }
     res.end();
     
