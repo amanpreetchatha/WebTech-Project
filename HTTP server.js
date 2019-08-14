4a01274a2dab886c0c2a9298ee61810bcf34b862
Amanpreet Chatha <50677671+amanpreetchatha@users.noreply.github.com>
2019-08-13 20:31:20 -0400

    fifth commit

--- HTTP server.js
+++ HTTP server.js
@@ -1,9 +1,47 @@
 const http=require('http');
+const fs=require('fs');
 
+let team=[];
 
 function handler(req,res){
     
-    const path=req.url;
+    if(req.method==='GET'){
+        if(req.url==='/team'){
+            if(team.length!=0){
+                res.write("<h1>Team Members</h1>");
+                res.write(`<li>${team}</li>`);
+                res.end();    
+            }
+            else{
+                res.write('<h2>No members in the team</h2>');
+                res.end();
+            }
+            
+        }
+        else{
+            file=fs.readFileSync('./index.html');
+            res.write(file);
+            res.end();
+        }
+    }else if(req.method==="POST"){
+        let body;
+        req.on('data',data=>{
+            name=data.toString();
+            console.log(data.toString());
+            team.push(name.slice(name.indexOf('=')+1,name.length));
+            
+        });
+        res.write('<h2>Member added to the team</h2><br><a href="/team">View Members</a>');
+        res.end();
+        /*
+        if(member.length!=0){
+            team.push(member);
+            res.write('<h3>Member added</h3>');
+            res.end();
+        }*/
+        
+        res.end();
+    }
     
     res.end();
 }
@@ -11,4 +49,4 @@ function handler(req,res){
 
 const server=http.createServer(handler);
 
-server.listen(3000);
+server.listen(80);
